006370d5d3e6faa60a099f5e0e88328c711f06c3
the crazy scientist <30580060+crazytec@users.noreply.github.com>
2017-07-30 20:31:14 -0400

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,12 +1,16 @@
 @echo off
 title virtual world system
 call :text message "ver 1.0 (test version)" 
-call :text warning this is consol 1. Can you see?
+call :text warning "this is consol 1. Can you see?"
 pause
-call :text message You will see the main process here.
-call :text message now, we are going to create random size of the world.
+call :text message "You will see the main process here."
+call :text message "now, we are going to create random size of the world."
 
-call :text message starting framework...
+call :text message "starting framework..."
 start framework.bat
 :text
-::message,warning,error,success
+::message,warning,error
+if "%1"=="message" (color 07&echo %2)
+if "%1"=="warning" (color 1E&echo %2)
+if "%1"=="error" (color 40&echo %2)
+::command should end here.
