c442f4c0e8b310f1412f3a3326d54182b54c5caa
the crazy scientist <30580060+crazytec@users.noreply.github.com>
2017-07-30 20:41:47 -0400

    Update main.bat

--- main.bat
+++ main.bat
@@ -5,8 +5,9 @@ call :text message "ver 1.0 (test version)"
 call :text warning "this is consol 1. Can you see?"
 pause
 call :text message "You will see the main process here."
+call :text message "loading setting..."
+for 
 call :text message "now, we are going to create random size of the world."
-set 
 call :text message "starting framework..."
 start framework.bat
 :text
