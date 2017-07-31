0d9977530585bb2568a55782c5e30fd2a568ba54
the crazy scientist <30580060+crazytec@users.noreply.github.com>
2017-07-30 20:36:57 -0400

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,11 +1,12 @@
 @echo off
 title virtual world system
+::tmperary variable: tmp
 call :text message "ver 1.0 (test version)" 
 call :text warning "this is consol 1. Can you see?"
 pause
 call :text message "You will see the main process here."
 call :text message "now, we are going to create random size of the world."
-
+set 
 call :text message "starting framework..."
 start framework.bat
 :text
