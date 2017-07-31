777985063a5a46171be4864240307064779a7f32
crazytec <30580060+crazytec@users.noreply.github.com>
2017-07-30 20:11:47 -0400

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,11 +1,12 @@
 @echo off
 title virtual world system
 call :text message "ver 1.0 (test version)" 
-call :text message starting framework...
-start framework.bat
 call :text warning this is consol 1. Can you see?
 pause
 call :text message You will see the main process here.
-call :text start
+call :text message now, we are going to create random size of the world.
+
+call :text message starting framework...
+start framework.bat
 :text
 ::message,warning,error,success
