f4f3a054e7281a88f08facc64bb2d04060fb0557
the crazy scientist <30580060+crazytec@users.noreply.github.com>
2017-08-01 19:41:09 -0400

    Update main.bat
    re-write it.(Again. It's the 3rd time.)

--- main.bat
+++ main.bat
@@ -1,38 +1,40 @@
 @echo off
 title virtual world system
-call :text message "ver 1.0 (test version)" 
-call :text warning "this is consol 1. Can you see?"
-pause
-call :text message "You will see the main process here."
-call :text message "loading setting..."
-if not exist setting.ini goto setting
+echo ver 1.0 beta 2 (editing...)
+echo this is consol 1.
+echo detecting setting file...
+if not exist setting.ini goto setnew
+echo setting file detected.
+echo loading setting file...
 for /f %%i in (setting.ini) do (set %%i)
-call :text message "Everything should be fine now (as long as you didn't edit other file)."
-if "%lastai%"=="" goto allnew
-call :text message "ok, AI finded."
-call :text message "now, we are going to create random size of the world."
-:xmax
-set x_max=%random%
-call :text message "trying using %x_max% for x max number."
-::maxx and maxy is the mexium places in setting.
-if %x_max% gtr %maxx% goto xmaxe
-:ymax
-set y_max=%random%
-call :text message "trying using %x_max% for y max number."
-::maxx and maxy is the mexium places in setting.
-if %y_max% gtr %maxy% goto ymaxe
-call :text message "starting framework..."
-start framework.bat
-exit
-:xmaxe
-call :text warning "%x_max% is too big."
-goto xmax
-:ymaxe
-call :text warning "%x_max% is too big."
-goto ymax
-:text
-::message,warning,error
-if "%1"=="message" (color 07&echo %2)
-if "%1"=="warning" (color 1E&echo %2)
-if "%1"=="error" (color 40&echo %2)
-::command should end here.
+if not exist sav_%lastok%.ai goto genstai
+copy sav_%lastok%.ai stai.ai
+echo generating diffculty...
+echo generating dimension...
+:gen_di
+set dimension=%random%
+if %dimension%==1 goto gen_di
+if %dimension% GTR %max_dimen% (echo %dimension% seems too big for dimensipn. Trying again...&goto gen_di)
+echo generating axis on each dimension
+:gen_axis
+set a=1
+:gen_axie
+set b=%random%
+set axis_%a%=%b%
+if %b% GTR %max_axis% goto gen_axie
+echo %b% seems ok for axis %a%.
+if %a%==%dimension% goto gen_axid
+set /a a=%a%+1
+goto gen_axie
+:gen_axid
+echo generating success mechanism...
+set a=%random%
+set /a success=%a%%%2
+if %success%==0 (echo success machanism:hungry game mode.)
+if %success%==1 (echo success machanism:most energy mode.)
+if %success%==2 (echo success machanism:most cell mode.)
+echo generating energy difficulty...
+set /a engy_diff=%random%+1
+set /a engy_difg=%engy_diff%*10000/32768
+echo energy difficulty:0.000%energy_difg%
+if %success%==0 (goto maxtimede)
