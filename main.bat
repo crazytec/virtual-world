bccb72892b80b3555c91b1fedd9856e05d260d4c
the crazy scientist <30580060+crazytec@users.noreply.github.com>
2017-07-31 20:01:24 -0400

    Update main.bat
    add x-max and y-max in the program.

--- main.bat
+++ main.bat
@@ -1,15 +1,35 @@
 @echo off
 title virtual world system
-::tmperary variable: tmp
 call :text message "ver 1.0 (test version)" 
 call :text warning "this is consol 1. Can you see?"
 pause
 call :text message "You will see the main process here."
 call :text message "loading setting..."
-for 
+if not exist setting.ini goto setting
+for /f %%i in (setting.ini) do (set %%i)
+call :text message "Everything should be fine now (as long as you didn't edit other file)."
+if "%lastai%"=="" goto allnew
+call :text message "ok, AI finded."
 call :text message "now, we are going to create random size of the world."
+:xmax
+set x_max=%random%
+call :text message "trying using %x_max% for x max number."
+::maxx and maxy is the mexium places in setting.
+if %x_max% gtr %maxx% goto xmaxe
+:ymax
+set y_max=%random%
+call :text message "trying using %x_max% for y max number."
+::maxx and maxy is the mexium places in setting.
+if %y_max% gtr %maxy% goto ymaxe
 call :text message "starting framework..."
 start framework.bat
+exit
+:xmaxe
+call :text warning "%x_max% is too big."
+goto xmax
+:ymaxe
+call :text warning "%x_max% is too big."
+goto ymax
 :text
 ::message,warning,error
 if "%1"=="message" (color 07&echo %2)
